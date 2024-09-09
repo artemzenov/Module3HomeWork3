f2ad4f2697caac3b9dfadeace6066ee95f781dbe
artemzenov <113045342+artemzenov@users.noreply.github.com>
2024-09-09 15:25:21 +0500

    Create module_3_3.py

---
+++ module_3_3.py
@@ -0,0 +1,29 @@
+print(f'1.Функции с параметрами по умолчанию:')
+
+def print_params(
+        a=1, 
+        b='string', 
+        c=True
+    ):
+    
+    print(a, b, c)
+
+
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+print_params(10, 'test', False)
+
+print(f'\n2.Распаковка параметров:')
+
+values_list = [2, 'test', 3.5]
+values_dict = dict(a='abc', b=25, c=False)
+
+print_params(*values_list)
+print_params(**values_dict)
+
+print(f'\n3.Распаковка + отдельные параметры:')
+
+values_list_2 = ['text', 3.14]
+
+print_params(*values_list_2, 42)
